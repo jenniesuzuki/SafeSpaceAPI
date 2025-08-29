fa0f8fe0d133f3a410224cc080d8603dd6503f34
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-29 09:07:54 -0300

    Alterando banco de dados

--- Migrations/20250829120537_InitialCreateSqlServer.Designer.cs
+++ Migrations/20250829120537_InitialCreateSqlServer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SafeSpaceAPI.Infrastructure.Context;
 
@@ -11,9 +12,11 @@ using SafeSpaceAPI.Infrastructure.Context;
 namespace SafeSpaceAPI.Migrations
 {
     [DbContext(typeof(SafeSpaceContext))]
-    partial class SafeSpaceContextModelSnapshot : ModelSnapshot
+    [Migration("20250829120537_InitialCreateSqlServer")]
+    partial class InitialCreateSqlServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250829120537_InitialCreateSqlServer.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SafeSpaceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateSqlServer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TIPO",
+                table: "UsuarioSS",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TELEFONE",
+                table: "UsuarioSS",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SENHA",
+                table: "UsuarioSS",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NOME",
+                table: "UsuarioSS",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EMAIL",
+                table: "UsuarioSS",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataCadastro",
+                table: "UsuarioSS",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "TIMESTAMP(7)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "UsuarioSS",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "RAW(16)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DESCRICAO",
+                table: "SolicitacoesAjuda",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATA_SOLICITACAO",
+                table: "SolicitacoesAjuda",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "TIMESTAMP(7)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "SolicitacoesAjuda",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "RAW(16)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UsuarioSSId",
+                table: "Agendamento",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "RAW(16)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DESCRICAO",
+                table: "Agendamento",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATA_AGENDAMENTO",
+                table: "Agendamento",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "TIMESTAMP(7)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Agendamento",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "RAW(16)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TIPO",
+                table: "UsuarioSS",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TELEFONE",
+                table: "UsuarioSS",
+                type: "NVARCHAR2(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SENHA",
+                table: "UsuarioSS",
+                type: "NVARCHAR2(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NOME",
+                table: "UsuarioSS",
+                type: "NVARCHAR2(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EMAIL",
+                table: "UsuarioSS",
+                type: "NVARCHAR2(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "DataCadastro",
+                table: "UsuarioSS",
+                type: "TIMESTAMP(7)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "UsuarioSS",
+                type: "RAW(16)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DESCRICAO",
+                table: "SolicitacoesAjuda",
+                type: "NVARCHAR2(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "DATA_SOLICITACAO",
+                table: "SolicitacoesAjuda",
+                type: "TIMESTAMP(7)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "SolicitacoesAjuda",
+                type: "RAW(16)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UsuarioSSId",
+                table: "Agendamento",
+                type: "RAW(16)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DESCRICAO",
+                table: "Agendamento",
+                type: "NVARCHAR2(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "DATA_AGENDAMENTO",
+                table: "Agendamento",
+                type: "TIMESTAMP(7)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Agendamento",
+                type: "RAW(16)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+    }
+}
